a23d1de67122d94be04f23fbd9191d32bb9a5320
Bohdana3007 <bohdanakushniruk1@gmail.com>
2020-12-22 18:58:38 +0200

    add homework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component, Fragment } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Section from './components/Section';
+import Statistics from './components/Statistics';
+import FeedbackOptions from './components/FeedbackOptions';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleButtonClick = ({ currentTarget: { value } }) => {
+    this.setState(prevState => {
+      return { [value]: prevState[value] + 1 };
+    });
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, value) => acc + value, 0);
+  };
+
+  countPositiveFeedbackPercentage = totalFeedbacks => {
+    const res = Math.round((this.state.good / totalFeedbacks) * 100);
+    return isNaN(res) ? 0 : res;
+  };
+
+  render() {
+    const totalFeedbacks = this.countTotalFeedback();
+    const positiveFeedbacksPercentage = this.countPositiveFeedbackPercentage(
+      totalFeedbacks,
+    );
+    return (
+      <Fragment>
+        <Section title="Please leave Feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.handleButtonClick}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            state={this.state}
+            total={totalFeedbacks}
+            positivePercentage={positiveFeedbacksPercentage}
+          />
+        </Section>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
